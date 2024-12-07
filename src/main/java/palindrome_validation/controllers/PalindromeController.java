79a9eb16b46ddb44dc915b7be54beb02f2092687
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-12-06 20:50:31 -0500

    fix: Change the return value of the function to boolean value

--- src/main/java/palindrome_validation/controllers/PalindromeController.java
+++ src/main/java/palindrome_validation/controllers/PalindromeController.java
@@ -2,19 +2,21 @@ package palindrome_validation.controllers;
 
 public class PalindromeController {
 
-    public void verifyPalindrome(){
-        String word="hola";
+
+
+    public boolean verifyPalindrome(String word){
         int i=0,j;
-        boolean isPalindrome=true;
         j=word.length()-1;
-        while (i==j){
+        while (i<j){
             if (word.charAt(i)!=word.charAt(j)){
-                isPalindrome=false;
-                break;
+                return  false;
             }
             i++;
             j--;
         }
+    return true;
     }
+
+
 
 }
